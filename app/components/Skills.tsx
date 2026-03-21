13c13df00e27fc7e3bc8cc0ce8fe1ec3f8a656fd
Nicolettastr <nicolettastruggia@gmail.com>
2026-03-21 15:44:01 +0100

    adding skills section

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,35 +1,103 @@
 "use client";
 import { useRef } from "react";
-import styles from "../styles/about.module.css";
+import {
+  SiCss,
+  SiHtml5,
+  SiJavascript,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPostgresql,
+  SiReact,
+  SiReactquery,
+  SiTypescript,
+} from "react-icons/si";
+import { TbStack } from "react-icons/tb";
+import styles from "../styles/skills.module.css";
 
 export const Skills = () => {
   const container = useRef<HTMLElement>(null);
   const wrapper = useRef<HTMLDivElement>(null);
 
   const languages = [
-    "HTML5",
-    "CSS3",
-    "JAVASCRIPT",
-    "REACT",
-    "NEXT.JS",
-    "TYPESCRIPT",
-    "NODE.JS",
-    "ZUSTAND",
-    "REACT QUERY",
+    {
+      name: "HTML5",
+      logo: <SiHtml5 className={styles.icon} />,
+    },
+    {
+      name: "CSS3",
+      logo: <SiCss className={styles.icon} />,
+    },
+    {
+      name: "JAVASCRIPT",
+      logo: <SiJavascript className={styles.icon} />,
+    },
+    {
+      name: "REACT",
+      logo: <SiReact className={styles.icon} />,
+    },
+    {
+      name: "NEXT.JS",
+      logo: <SiNextdotjs className={styles.icon} />,
+    },
+    {
+      name: "TYPESCRIPT",
+      logo: <SiTypescript className={styles.icon} />,
+    },
+    {
+      name: "NODE.JS",
+      logo: <SiNodedotjs className={styles.icon} />,
+    },
+    {
+      name: "ZUSTAND",
+      logo: <TbStack className={styles.icon} />,
+    },
+    {
+      name: "REACT QUERY",
+      logo: <SiReactquery className={styles.icon} />,
+    },
+    { name: "POSTGRESQL", logo: <SiPostgresql className={styles.icon} /> },
   ];
 
-  const tools = ["GIT", "VITE", "FIGMA", "POSTMAN", "DBEAVER"];
+  const tools = [
+    "GIT",
+    "VITE",
+    "FIGMA",
+    "POSTMAN",
+    "DBEAVER",
+    "VS CODE",
+    "JIRA",
+    "SCRUM",
+    "GITHUB",
+    "GITLAB",
+  ];
 
   const languagesDisplay = languages.map((lang) => {
-    return <span key={lang}></span>;
+    return (
+      <div className={styles.lang_wrapper} key={lang.name}>
+        {lang.logo}
+        <p>{lang.name}</p>
+      </div>
+    );
+  });
+
+  const toolsDisplay = tools.map((tool) => {
+    return (
+      <div key={tool} className={styles.tool}>
+        <p>{tool}</p>
+      </div>
+    );
   });
 
   return (
     <section ref={container} className={styles.container}>
+      <h2 className={styles.title}>Skills and tools</h2>
       <div ref={wrapper} className={styles.wrapper}>
-        <p>Skills</p>
-        <h2>Technologies I work with</h2>
-        <p>Languages and Frameworks</p>
+        <section className={styles.section_lang}>
+          <div className={styles.tech_wrapper}>{languagesDisplay}</div>
+        </section>
+        <section className={styles.section_tools}>
+          <div className={styles.tools_wrapper}>{toolsDisplay}</div>
+        </section>
       </div>
     </section>
   );
